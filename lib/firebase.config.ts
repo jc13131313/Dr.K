d1242b061ee154e1b83547cd09dc012246b83f9d
JCreate <152024892+jc13131313@users.noreply.github.com>
2024-12-04 23:28:41 +0800

    Update firebase.config.ts

--- lib/firebase.config.ts
+++ lib/firebase.config.ts
@@ -4,13 +4,13 @@ import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpzAE63osPiGVVf49_Q1u6zfvSOs-m5hk",
-  authDomain: "dr-k-d071a.firebaseapp.com",
-  projectId: "dr-k-d071a",
-  storageBucket: "dr-k-d071a.appspot.com",
-  messagingSenderId: "899308291991",
-  appId: "1:899308291991:web:b6091dcea21f1dd78a26d2",
-  measurementId: "G-9PXFWE07RT"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
